0d5b49c8ce1cdc537edd18f227e95db9c186d018
Mohnish <mohnish.a007@gmail.com>
2024-07-27 20:41:09 +0100

    React material UI login and signup at center

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Base from './components/Base';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Userreg from './pages/Userreg';
 function App() {
   
   return (
     < >
      
         {/* <Base/> */}
-        <Login/>
+        {/* <Login/> */}
       {/* <Signup/> */}
+        <Userreg/>
+
     </>
   );
 }
--- src/pages/Userreg.jsx
+++ src/pages/Userreg.jsx
@@ -1,13 +1,34 @@
 import React, { useState } from 'react'
 import Login from '../components/Login'
 import Signup from '../components/Signup'
+import { Box } from '@mui/material'
+import styled from '@emotion/styled'
 
 function Userreg() {
-    const [user,setUser]=useState('login')
+    const Loginpage = styled(Box)`
+       display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+ 
+ 
+    `
+    const Container = styled(Box)`
+    display: flex;
+justify-content: center;
+align-items: center;
+ height: 100vh;
+
+ `
+
   return (
     <>
-    {user === 'login'? <Login/>:<Signup/>}
-   
+   <Container>
+   <Loginpage>
+    <Login/>
+   </Loginpage>
+   </Container>
+    
    
     </>
   )
