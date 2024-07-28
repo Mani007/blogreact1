75b4530610f318272d5a306f6e64cb5dcbd90180
Mohnish <mohnish.a007@gmail.com>
2024-07-28 17:25:45 +0100

    Api and and signup working fine

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -2,17 +2,29 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser')
-
-const signupUser = (req,res) =>{
+const user = require('../models/userModels')
+const signupUser = async(req,res) =>{
     // code for signup logic
-    //get the signup parameter from the body
+    try {
+        //get the signup parameter from the body
     const { email, password} = req.body;
     // check if user already exists
+    const User = await user.findOne({ email });
+    if (User) return res.status(400).json({message:'email already registered'}); // check if email already exists
+    // hasing the password 
+    const hashedPassword = await bcrypt.hash(password, 10);  // 10 is the number of rounds for hashing
+    // create user in the database
+    await user.create({ email,password: hashedPassword });
 
-    console.log(email, password)
-    // if user dont exist, create the user in the database
-    // hash the password using bcrypt
+    // respond after successful creation 
     res.sendStatus(200)
+    
+    
+    } catch(err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+    
 
 }
 
