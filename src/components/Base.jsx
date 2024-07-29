8f77ba0cf8ce86a88bf5104f02eefa49a9e3c26e
Mohnish <mohnish.a007@gmail.com>
2024-07-29 12:44:00 +0100

    Base NOT working as multiple input field

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -4,15 +4,20 @@ import { TextField } from '@mui/material';
 import { useState } from 'react';
 
 export default function ButtonUsage() {
-  const [inter,setInter] = useState('');
+  const [inter,setInter] = useState({
+    input1:'',
+    input2: '',
+  });
   const interdis = (e) => {
-    setInter(e.target.value);
+    setInter({...inter, [e.target.name]:e.target.value});
   }
   // return <Button variant="contained">Hello world</Button>;
   return (
     <>
-    <h1>The value of input is {inter}</h1>
+    <h1>The value of input is {inter.input1}</h1>
+    <h1>The value of input111 is {inter.input2}</h1>
     <TextField variant='outlined' onChange={interdis} name='statement' placeholder='abc' label='some text'/>
+    <TextField variant='outlined' onChange={interdis} name='statement1' placeholder='abc1' label='some text 1'/>
     </>
   )
 }
